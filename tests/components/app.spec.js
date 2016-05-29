c5a878290b5ca907777de30947a55d9d32c50647
syropian <collinpatrickhenderson@gmail.com>
2016-05-29 10:43:29 -0400

    Use ES6 style for app spec

--- tests/components/app.spec.js
+++ tests/components/app.spec.js
@@ -1,12 +1,13 @@
+import App from "../../src/js/components/app.vue";
+
 describe("App Component", function () {
-  var App = require("../../src/js/components/app.vue");
-  it("should have a name", function(){
+  it("should have a name", () => {
     expect(App.name).toBe("App");
   });
-  it("should have a Vuex store", function(){
+  it("should have a Vuex store", () => {
     expect(App.hasOwnProperty("store")).toBe(true);
   });
-  it("should have a router view in its template", function(){
+  it("should have a router view in its template", () => {
     expect(App.template).toMatch(/<router-view\s*(.*)><\/router-view>/i)
   });
 })
